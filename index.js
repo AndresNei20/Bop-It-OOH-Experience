914e1f0502c47126f8bc58427a6c9eb04425ccac
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2023-10-17 20:40:12 -0500

    screens: turn y scream

--- index.js
+++ index.js
@@ -56,16 +56,90 @@ class Main {
       
     }
 
+  //paola - pantalla scream
+
+  class Scream {
+
+    constructor (){
+      this.elipse = loadImage('img/yellowElipse.png');
+      this.purpleCircle = loadImage('img/semicirclep.png');
+      this.number = loadImage('img/1.png');
+      this.backgroundSplash = loadImage('img/purplesplurt.png');
+      this.microphone = loadImage('img/mic.png');
+      this.scream = loadImage('img/scream.png');
+
+    }
+
+    draw(){
+      background('black');
+
+        image(this.elipse, 157,8,100,95)
+        image(this.purpleCircle, 102,20,210,110)
+        image(this.number, 196,35, 15, 30)
+        image(this.backgroundSplash, 20,30)
+        image(this.scream, -10,670, 430, 224)
+        image(this.microphone, 110,410, 200, 300)
+       
+
+
+    }
+
+  }
+
+  class Turn {
+
+    constructor (){
+      this.elipse = loadImage('img/yellowElipse.png');
+      this.purpleCircle = loadImage('img/semicirclep.png');
+      this.number = loadImage('img/1.png');
+      this.backgroundSplash = loadImage('img/purplesplurt.png');
+      this.bigArrow = loadImage('img/bigarrow.png');
+      this.turn = loadImage('img/turn.png');
+      this.blueArrow = loadImage('img/bluearrow.png');
+      this.redArrow = loadImage('img/redarrow.png');
+      this.purpleArrow = loadImage('img/purplearrow.png');
+
+    }
+
+    draw(){
+      background('black');
+
+// margin left / margin top / width / height
+
+        image(this.elipse, 157,8,100,95)
+        image(this.purpleCircle, 102,20,210,110)
+        image(this.number, 196,35, 15, 30)
+        image(this.backgroundSplash, 20,30)
+        image(this.bigArrow, 0,255, 400, 400)
+        image(this.turn, 110,494, 185, 100)
+        image(this.blueArrow, 50,220, 180, 150)
+        image(this.redArrow, 50,600, 150, 150)
+        image(this.purpleArrow, 334,570, 80, 80)
+       
+
+
+    }
+
+  }
+
+
+
   
   // sketch.js (archivo principal)
   let home;
   let main;
   let pantallaActual;
 
+  
+  let scream; // pantalla scream pao
+  let turn; // pantalla turn pao
+
   function setup() {
     createCanvas(414, 896);
     home = new Home();
     main = new Main();
+    scream = new Scream(); // iniciar pantalla scream
+    turn = new Turn(); //iniciar turn pao
     pantallaActual = home; // Iniciar en la pantalla 1
   }
   
@@ -80,6 +154,13 @@ class Main {
       } else if (key === '2') {
         pantallaActual = main;
       }
+      else if (key === '4') {
+        pantallaActual = scream;
+      } // pantalla 4 scream
+
+      else if (key === '5') {
+        pantallaActual = turn;
+      } // pantalla 5 turn
     }
   }
 
