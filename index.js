da758084c12e470b49cb94760db9362467bd70a2
AndresNei20 <andresfelipeneira2004@gmail.com>
2023-10-19 10:03:26 -0500

    Shake class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Home } from './screens/Home.js';
 import { Main } from './screens/Main.js';
+import { Shake } from './screens/Shake.js';
 import { DataUser } from './screens/DataUser.js';
 
 
@@ -7,21 +8,60 @@ const app = p5 => {
   let home;
   let main;
   let dataUser;
+  let shake; //shake screen
   let currentScreen;
+
+  let startingTime = 10;// el timer empezara desde 60 segundos
+  let lastUpdateTime = 0;
+  let currentDisplayTime = startingTime; // Tiempo que se muestra actualmente 
+  let timeStarted = false; //indica si el temporizador ya esta activo
+  let timerVisible = true; // Controla la visibilidad del temporizador
 
   p5.setup = function() {
     p5.createCanvas(414, 896);
     dataUser = new DataUser(p5);
     home = new Home(p5, currentScreen, dataUser);
     
-   
+    shake = new Shake(p5)
+
     main = new Main(p5);
     currentScreen = home; 
+    lastUpdateTime = p5.millis(); // Inicializa el último tiempo de actualización
   }
   
   p5.draw = function() {
     p5.background(0);
     currentScreen.show(p5);
+
+ 
+    if (currentScreen === main && !timeStarted) {
+      // Inicia el temporizador cuando currentScreen sea igual a main y no se haya iniciado antes
+      timeStarted = true;
+      console.log("Comienza el temporizador");
+    }
+
+    // Timer function 
+    if (timeStarted) {
+      const currentTime = p5.millis();
+      if (currentTime - lastUpdateTime >= 1000) {
+        // Actualiza el contador de tiempo cada segundo
+        if (currentDisplayTime > 0) {
+          currentDisplayTime -= 1;
+        } else {
+          console.log("¡Tiempo agotado!");
+          timerVisible = false; // Oculta el temporizador
+          timeStarted = false; // Detiene el temporizador
+          currentDisplayTime = 0 | undefined; // Asegura que el tiempo sea 0
+        }
+        lastUpdateTime = currentTime;
+      }
+    }
+
+    p5.textSize(32);
+    p5.fill(255);
+    const displayTime = p5.int(currentDisplayTime);
+    p5.text(displayTime, p5.width / 2, 50);
+
   
     if (p5.keyIsPressed) {
       if (p5.key === '2') {
@@ -32,16 +72,18 @@ const app = p5 => {
       } else if (p5.key === '3') {
         currentScreen.hideInput();
         currentScreen = main;
+        timerVisible = true; // Vuelve a mostrar el temporizador
         currentScreen.showInput();
         console.log("Cambio a 3")
+      }else if (p5.key === '4') {
+        currentScreen.hideInput();
+        currentScreen = shake;
+        timerVisible = true; // Vuelve a mostrar el temporizador
+        currentScreen.showInput();
+        console.log("Cambio a 4")
       }
-  }
+    }
   }
 }
 
 new p5(app);
-
-  
-
-  
-
